e1b683ad1cd40325177edff53fb9d90e718a9c79
syaban <rizkisyaban2@gmail.com>
2022-07-28 23:53:07 +0700

    add basic route gin

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	router := gin.Default()
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"name": "Rizki Syaban Aryanto",
+			"bio":  "Software Engineer",
+		})
+	})
+
+	router.GET("/hello", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"title":    "Hello Wolrd",
+			"subtitle": "Belajar Golang API",
+		})
+	})
+
+	router.Run(":5000")
 }
