d2a9461143d0ce13dbf9d25bef79ac7e9adc23e8
syaban <rizkisyaban2@gmail.com>
2022-07-29 00:11:20 +0700

    add GET router param and query

--- main.go
+++ main.go
@@ -9,8 +9,9 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/", rootHandler)
-
 	router.GET("/hello", helloHandler)
+	router.GET("/books/:id/:title", booksHandler)
+	router.GET("/query", queryHandler)
 
 	router.Run(":5000")
 }
@@ -27,4 +28,16 @@ func helloHandler(ctx *gin.Context) {
 		"title":    "Hello World",
 		"subtitle": "Belajar Golang API",
 	})
+}
+
+func booksHandler(ctx *gin.Context) {
+	id := ctx.Param("id")
+	title := ctx.Param("title")
+	ctx.JSON(http.StatusOK, gin.H{"id": id, "title": title})
+}
+
+func queryHandler(ctx *gin.Context) {
+	id := ctx.Query("id")
+	title := ctx.Query("title")
+	ctx.JSON(http.StatusOK, gin.H{"id": id, "title": title})
 }
